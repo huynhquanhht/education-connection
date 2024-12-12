83ab3c633e0df2c7bb5c0f193ca69ff1c7ba9227
huynhquanhht <huynhquanhht@gmail.com>
2024-12-12 21:51:41 +0700

    Update validation pipe

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -1,36 +1,23 @@
-import { PipeTransform, Injectable, ArgumentMetadata, HttpException, HttpStatus } from '@nestjs/common';
-import { validate } from 'class-validator';
-import { instanceToPlain } from 'class-transformer';
+import {
+  ArgumentMetadata,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  ValidationPipe,
+} from '@nestjs/common';
 
 @Injectable()
-export class ValidationPipe implements PipeTransform<any> {
-  async transform(value: any, { metatype }: ArgumentMetadata) {
-    if (!metatype || !this.toValidate(metatype)) return value;
+export class CustomValidationPipe extends ValidationPipe {
+  async transform(value: any, metadata: ArgumentMetadata) {
+    const validationErrors = await super.transform(value, metadata);
 
-    const object = instanceToPlain(metatype, value);
-    const errors = await validate(object, {
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      stopAtFirstError: true,
-    });
-
-    if (errors.length > 0) {
+    if (Array.isArray(validationErrors) && validationErrors.length > 0) {
       throw new HttpException(
-        {
-          statusCode: HttpStatus.UNPROCESSABLE_ENTITY,
-          message: 'Unprocessable Entity',
-          errors: errors,
-        },
-        HttpStatus.UNPROCESSABLE_ENTITY,
+        validationErrors[0].message,
+        HttpStatus.BAD_REQUEST,
       );
     }
-    return instanceToPlain(object);
-  }
 
-  // eslint-disable-next-line
-  private toValidate(metatype: Function): boolean {
-    // eslint-disable-next-line
-    const types: Function[] = [String, Boolean, Number, Array, Object];
-    return !types.includes(metatype);
+    return value;
   }
 }

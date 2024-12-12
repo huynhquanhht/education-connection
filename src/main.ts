6a35d2fc3fc2c8f9087fe8dca24c1ab0a4bc4148
huynhquanhht <huynhquanhht@gmail.com>
2024-12-12 21:51:41 +0700

    Update validation pipe

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from '@/filter/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { CustomValidationPipe } from '@/pipes/validation.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
-    new ValidationPipe({
+    new CustomValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       stopAtFirstError: true,

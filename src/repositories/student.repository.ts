157c32ca1491b409899c45e948521d5b4380002a
huynhquanhht <huynhquanhht@gmail.com>
2024-12-12 21:30:32 +0700

    Update unit test for apis

--- src/repositories/student.repository.ts
+++ src/repositories/student.repository.ts
@@ -6,52 +6,64 @@ import { TeacherStudent } from '@/entities/teacher-student.entity';
 
 @Injectable()
 export class StudentRepository extends Repository<Student> {
-	constructor(private dataSource: DataSource) {
-		super(Student, dataSource.createEntityManager());
-	}
+  constructor(private dataSource: DataSource) {
+    super(Student, dataSource.createEntityManager());
+  }
 
-	getByEmail(email: string): Promise<Student> {
-		return this.findOne({
-			where: { email },
-		});
-	}
+  getByEmail(email: string): Promise<Student> {
+    return this.findOne({
+      where: { email },
+    });
+  }
 
-	getByEmails(emails: string[]): Promise<Student[]> {
-		return this.findBy({ email: In(emails) });
-	}
+  getByEmails(emails: string[]): Promise<Student[]> {
+    return this.findBy({ email: In(emails) });
+  }
 
-	suspendStudent(student: Student) {
-		this.createQueryBuilder().update().set({ isSuspended: true }).where({ id: student.id }).execute();
-	}
+  suspendStudent(student: Student) {
+    this.dataSource
+      .createQueryBuilder()
+      .from(Student, 's')
+      .update()
+      .set({ isSuspended: true })
+      .where({ id: student.id })
+      .execute();
+  }
 
-	getCommonStudents(listTeacher: Teacher[]): Promise<Student[]> {
-		return this.dataSource
-			.createQueryBuilder()
-			.select('*')
-			.from(Student, 's')
-			.innerJoin(
-				(sub) =>
-					sub
-						.select('ts.student_id')
-						.from(TeacherStudent, 'ts')
-						.where(`ts.teacher_id IN (${listTeacher.map((t) => t.id).join(',')})`)
-						.groupBy('ts.student_id')
-						.having(`COUNT(ts.student_id) = ${listTeacher.length}`),
-				'subQuery',
-				'subQuery.student_id = s.id',
-			)
-			.execute();
-	}
+  getCommonStudents(listTeacher: Teacher[]): Promise<Student[]> {
+    const createSubQuery = (sub: any) =>
+      sub
+        .select('ts.student_id')
+        .from(TeacherStudent, 'ts')
+        .where(`ts.teacher_id IN (${listTeacher.map((t) => t.id).join(',')})`)
+        .groupBy('ts.student_id')
+        .having(`COUNT(ts.student_id) = ${listTeacher.length}`);
+    return this.dataSource
+      .createQueryBuilder()
+      .select('*')
+      .from(Student, 's')
+      .innerJoin(
+        (sub) => createSubQuery(sub),
+        'subQuery',
+        'subQuery.student_id = s.id',
+      )
+      .execute();
+  }
 
-	async retrieveForNotifications(teacherId: number, mentionedStudents: string[]): Promise<Student[]> {
-		return this.createQueryBuilder('s')
-			.leftJoin('teacher_student', 'ts', 'ts.student_id = s.id')
-			.where('s.is_suspended is false')
-			.andWhere('(s.email in (:...emails) OR ts.teacher_id = :teacherId )', {
-				emails: [mentionedStudents],
-				teacherId: teacherId,
-			})
-			.select(['s.id', 's.email'])
-			.getMany();
-	}
+  retrieveForNotifications(
+    teacherId: number,
+    mentionedStudents: string[],
+  ): Promise<Student[]> {
+    return this.dataSource
+      .createQueryBuilder()
+      .select(['s.id', 's.email'])
+      .from(Student, 's')
+      .leftJoin('teacher_student', 'ts', 'ts.student_id = s.id')
+      .where('s.is_suspended is false')
+      .andWhere('(s.email in (:...emails) OR ts.teacher_id = :teacherId )', {
+        emails: [mentionedStudents],
+        teacherId: teacherId,
+      })
+      .getMany();
+  }
 }
--- src/repositories/tests/student.repository.spec.ts
+++ src/repositories/tests/student.repository.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { DataSource } from 'typeorm';
+import { DataSource, In } from 'typeorm';
 import { StudentRepository } from '@/repositories/student.repository';
 import { Teacher } from '@/entities/teacher.entity';
 import { Student } from '@/entities/student.entity';
@@ -11,6 +11,8 @@ describe('StudentRepository', () => {
   const mockCreateQueryBuilder = {
     select: jest.fn().mockReturnThis(),
     from: jest.fn().mockReturnThis(),
+    update: jest.fn().mockReturnThis(),
+    set: jest.fn().mockReturnThis(),
     innerJoin: jest.fn().mockReturnThis(),
     leftJoin: jest.fn().mockReturnThis(),
     where: jest.fn().mockReturnThis(),
@@ -47,17 +49,115 @@ describe('StudentRepository', () => {
       const teachers = [{ id: 1 }, { id: 2 }] as Teacher[];
       const students = [{ id: 1, email: 'student@example.com' }] as Student[];
 
-      // Set the execute function to resolve with our mock data
       mockCreateQueryBuilder.execute.mockResolvedValue(students);
 
       const result = await studentRepository.getCommonStudents(teachers);
 
-      // Verify the query building steps
       expect(mockCreateQueryBuilder.select).toHaveBeenCalledWith('*');
       expect(mockCreateQueryBuilder.from).toHaveBeenCalledWith(Student, 's');
       expect(mockCreateQueryBuilder.innerJoin).toHaveBeenCalled();
       expect(mockCreateQueryBuilder.execute).toHaveBeenCalled();
       expect(result).toEqual(students);
+    });
+
+    describe('getByEmail', () => {
+      it('should call findOne with the correct email', async () => {
+        const email = 'test@example.com';
+        const expectedResult = { id: 1, email };
+
+        studentRepository.findOne = jest.fn().mockResolvedValue(expectedResult);
+
+        const result = await studentRepository.getByEmail(email);
+
+        expect(studentRepository.findOne).toHaveBeenCalledWith({
+          where: { email },
+        });
+        expect(result).toEqual(expectedResult);
+      });
+    });
+
+    describe('getByEmails', () => {
+      it('should return a list of students by their emails', async () => {
+        const studentEmails = ['student1@example.com', 'student2@example.com'];
+        const students: Student[] = [
+          { email: studentEmails[0] } as Student,
+          { email: studentEmails[1] } as Student,
+        ];
+
+        studentRepository.findBy = jest.fn().mockResolvedValue(students);
+
+        const result = await studentRepository.getByEmails(studentEmails);
+
+        expect(studentRepository.findBy).toHaveBeenCalledWith({
+          email: In(studentEmails),
+        });
+        expect(result).toEqual(students);
+      });
+    });
+
+    describe('suspendStudent', () => {
+      it('should suspend a student', async () => {
+        const mockStudent = {
+          id: 1,
+          email: 'test@example.com',
+          isSuspended: true,
+        };
+
+        mockCreateQueryBuilder.execute.mockResolvedValue(1);
+
+        await studentRepository.suspendStudent(mockStudent);
+
+        expect(mockCreateQueryBuilder.update).toHaveBeenCalledTimes(1);
+        expect(mockCreateQueryBuilder.execute).toHaveBeenCalledTimes(1);
+        expect(mockCreateQueryBuilder.set).toHaveBeenCalledWith({
+          isSuspended: true,
+        });
+      });
+    });
+
+    describe('retrieveForNotifications', () => {
+      it('should return a list of students sent notifications', async () => {
+        const mentionedStudents = [
+          'student1@example.com',
+          'student2@example.com',
+        ];
+        const teacherId = 1;
+
+        const students: Student[] = [
+          { email: mentionedStudents[0] } as Student,
+          { email: mentionedStudents[1] } as Student,
+        ];
+
+        mockCreateQueryBuilder.getMany.mockResolvedValue(students);
+
+        const result = await studentRepository.retrieveForNotifications(
+          teacherId,
+          mentionedStudents,
+        );
+
+        expect(result).toEqual(students);
+        expect(mockCreateQueryBuilder.select).toHaveBeenCalledWith([
+          's.id',
+          's.email',
+        ]);
+        expect(mockCreateQueryBuilder.from).toHaveBeenCalledWith(Student, 's');
+        expect(mockCreateQueryBuilder.leftJoin).toHaveBeenCalledWith(
+          'teacher_student',
+          'ts',
+          'ts.student_id = s.id',
+        );
+        expect(mockCreateQueryBuilder.where).toHaveBeenCalledWith(
+          's.is_suspended is false',
+        );
+        expect(mockCreateQueryBuilder.andWhere).toHaveBeenCalledWith(
+          '(s.email in (:...emails) OR ts.teacher_id = :teacherId )',
+          {
+            emails: [mentionedStudents],
+            teacherId: teacherId,
+          },
+        );
+        expect(mockCreateQueryBuilder.getMany).toHaveBeenCalledTimes(1);
+      });
     });
   });
 });
